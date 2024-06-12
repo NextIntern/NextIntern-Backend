6a0ea3e747289cdf1bf517f53fa437c52399e72e
iamdwn <truongdinhdongduong@gmail.com>
2024-06-13 01:53:41 +0700

    feat: add CR University

--- SWD.NextIntern.Service/DTOs/Responses/ResponseObject.cs
+++ SWD.NextIntern.Service/DTOs/Responses/ResponseObject.cs
@@ -1,11 +1,25 @@
-﻿
+﻿using System.Net;
 
 namespace SWD.NextIntern.Service.DTOs.Responses
 {
-    public class ResponseObject
+    public class ResponseObject<T>
     {
-        public string status;
-        public string message;
-        public object data;
+        public T? Data { get; set; }
+        public HttpStatusCode Status { get; set; }
+        public string Message { get; set; }
+
+        public ResponseObject(T? data, HttpStatusCode status, string message)
+        {
+            Data = data;
+            Status = status;
+            Message = message;
+        }
+
+        public ResponseObject(HttpStatusCode status, string message)
+        {
+            Data = default;
+            Status = status;
+            Message = message;
+        }
     }
 }
