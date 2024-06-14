b510d3cb90c844cb273ac7872148d986db09e952
iamdwn <truongdinhdongduong@gmail.com>
2024-06-14 10:48:05 +0700

    fix: fix auth

--- SWD.NextIntern.Service/Services/Auth/ForgotPassword/ForgotPasswordQueryHandler.cs
+++ SWD.NextIntern.Service/Services/Auth/ForgotPassword/ForgotPasswordQueryHandler.cs
@@ -25,7 +25,8 @@ namespace SWD.NextIntern.Service.Auth.ForgotPassword
 
         public async Task<string> Handle(ForgotPasswordQuery request, CancellationToken cancellationToken)
         {
-            if (_userRepository.FindAsync(i => i.Email.Equals(request.Email)) == null)
+            var user = await _userRepository.FindAsync(i => request.Email.Equals(i.Email));
+            if (!(user != null))
             {
                 throw new Exception("Email is not registed.");
             }
