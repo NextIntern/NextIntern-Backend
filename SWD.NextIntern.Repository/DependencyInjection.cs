42db99b20911178751b1c3aeedde930b0569e0a3
handgun <DungPHASE172122@fpt.edu.vn>
2024-06-15 18:52:54 +0700

    Delete RoleRepository

--- SWD.NextIntern.Repository/DependencyInjection.cs
+++ SWD.NextIntern.Repository/DependencyInjection.cs
@@ -33,7 +33,6 @@ namespace SWD.NextIntern.Repository
             services.AddScoped<IUniversityRepository, UniversityRepository>();
             services.AddScoped<ICampaignEvaluationRepository, CampaignEvaluationRepository>();
             services.AddScoped<IInternEvaluationRepository, InternEvaluationRepository>();
-            //services.AddTransient<IRoleRepository, RoleRepository>();
             services.AddTransient<IEvaluationFormRepository, EvaluationFormRepository>();
 
 
