545f626168ab640ef457846071fbef720068e9f1
iamdwn <truongdinhdongduong@gmail.com>
2024-06-17 19:25:41 +0700

    fix: fix CRUD University

--- SWD.NextIntern.API/Controllers/University/UniversityController.cs
+++ SWD.NextIntern.API/Controllers/University/UniversityController.cs
@@ -1,11 +1,13 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SWD.NextIntern.Service.DTOs.Responses;
-using SWD.NextIntern.Service.Services.CampaignService.GetAll;
-using SWD.NextIntern.Service.Services.CampaignService;
-using SWD.NextIntern.Service.Services.CampaignService.GetById;
 using SWD.NextIntern.Service.Services.UniversityService;
 using SWD.NextIntern.Service.Services.UniversityService.GetById;
+using SWD.NextIntern.Service.Services.UniversityService.Create;
+using SWD.NextIntern.Service.Services.UniversityService.Update;
+using SWD.NextIntern.Service.Services.UniversityService.Delete;
+using SWD.NextIntern.Service.Services.UniversityService.Create;
+using SWD.NextIntern.Service.Services.UniversityService.GetAll;
 
 namespace SWD.NextIntern.API.Controllers.University
 {
@@ -20,10 +22,38 @@ namespace SWD.NextIntern.API.Controllers.University
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
+        [HttpPost("create")]
+        public async Task<ResponseObject<string>> CreateUniversity([FromBody] CreateUniversityCommand command, CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            return result;
+        }
+
+        [HttpGet("all")]
+        public async Task<ResponseObject<List<UniversityDto>>> GetAllUniversity(CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(new GetAllQuery(), cancellationToken);
+            return result;
+        }
+
         [HttpGet("{id}")]
-        public async Task<ResponseObject<UniversityDto?>> GetCampaignById(string id, CancellationToken cancellationToken = default)
+        public async Task<ResponseObject<UniversityDto>> GetUniversityById(string id, CancellationToken cancellationToken = default)
         {
             var result = await _mediator.Send(new GetUniversityByIdQuery(id), cancellationToken);
+            return result;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ResponseObject<string>> DeleteUniversity(string id, CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(new DeleteUniversityCommand(id), cancellationToken);
+            return result;
+        }
+
+        [HttpPut("update")]
+        public async Task<ResponseObject<string>> UpdateUniversity([FromBody] UpdateUniversityCommand command, CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
             return result;
         }
     }
