086afc9e696826afcdb5b4c5de1bf497b406ba6d
iamdwn <truongdinhdongduong@gmail.com>
2024-07-18 23:38:55 +0700

    fix: fix create Intern

--- SWD.NextIntern.Service/Services/InternService/Create/CreateInternCommand.cs
+++ SWD.NextIntern.Service/Services/InternService/Create/CreateInternCommand.cs
@@ -22,16 +22,16 @@ namespace SWD.NextIntern.Service.InternService.Create
             ImgUrl = imgUrl;
         }
 
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string ConfirmedPassword { get; set; }
-        public string Fullname { get; set; }
-        public string Email { get; set; }
-        public string Gender { get; set; }
-        public string Telephone { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmedPassword { get; set; }
+        public string? Fullname { get; set; }
+        public string? Email { get; set; }
+        public string? Gender { get; set; }
+        public string? Telephone { get; set; }
         public DateOnly? Dob { get; set; }
         public string RoleName { get; set; }
-        public string Address { get; set; }
+        public string? Address { get; set; }
         public string ImgUrl { get; set; }
     }
 }
