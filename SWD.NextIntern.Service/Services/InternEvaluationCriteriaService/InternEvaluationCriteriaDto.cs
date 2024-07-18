9aada7e7a76994f2e38b49adfd8c9aa39138d47b
handgun <DungPHASE172122@fpt.edu.vn>
2024-07-18 23:26:54 +0700

    fix dto

--- SWD.NextIntern.Service/Services/InternEvaluationCriteriaService/InternEvaluationCriteriaDto.cs
+++ SWD.NextIntern.Service/Services/InternEvaluationCriteriaService/InternEvaluationCriteriaDto.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using SWD.NextIntern.Repository.Entities;
 using SWD.NextIntern.Service.Common.Mappings;
+using SWD.NextIntern.Service.Services.InternEvaluationService;
 using System.Text.Json.Serialization;
 
 public class InternEvaluationCriteriaDto : IMapFrom<InternEvaluationCriterion>
@@ -9,9 +10,9 @@ public class InternEvaluationCriteriaDto : IMapFrom<InternEvaluationCriterion>
 
     public string Id { get; set; }
 
-    public InternEvaluation InternEvaluation { get; set; }
+    public InternEvaluationDto InternEvaluationDto { get; set; }
 
-    public FormCriterion FormCriteria { get; set; }
+    public FormCriteriaDto FormCriteriaDto { get; set; }
 
     public int Score { get; set; }
 
@@ -20,6 +21,8 @@ public class InternEvaluationCriteriaDto : IMapFrom<InternEvaluationCriterion>
 
     public void Mapping(Profile profile)
     {
-        profile.CreateMap<InternEvaluationCriterion, InternEvaluationCriteriaDto>();
+        profile.CreateMap<InternEvaluationCriterion, InternEvaluationCriteriaDto>()
+            .ForMember(dest => dest.InternEvaluationDto, opt => opt.MapFrom(src => src.InternEvaluation))
+            .ForMember(dest => dest.FormCriteriaDto, opt => opt.MapFrom(src => src.FormCriteria));
     }
 }
