c498b0ef959e1009eb80f9c48c1730d7980d5a0b
handgun <DungPHASE172122@fpt.edu.vn>
2024-07-20 03:20:47 +0700

    add

---
+++ SWD.NextIntern.Service/Services/EvaluationFormService/GetByUniversityId/GetEvaluationFormByUniversityIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SWD.NextIntern.Service.DTOs.Responses;
+
+namespace SWD.NextIntern.Service.Services.EvaluationFormService.GetByUniversityId
+{
+    public class GetEvaluationFormByUniversityIdQuery : IRequest<ResponseObject<PagedListResponse<EvaluationFormDto>>>
+    {
+        public string UniversityId { get; set; }
+        public int PageSize { get; set; }
+        public int PageNo { get; set; }
+
+        public GetEvaluationFormByUniversityIdQuery(string universityId, int pageNo, int pageSize)
+        {
+            UniversityId = universityId;
+            PageNo = pageNo;
+            PageSize = pageSize;
+        }
+    }
+}
