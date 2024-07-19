274becd35f6eeb1436bea235254d750b21988172
handgun <DungPHASE172122@fpt.edu.vn>
2024-07-19 08:17:14 +0700

    fix delete campaign

--- SWD.NextIntern.Repository/Persistence/AppDbContext.cs
+++ SWD.NextIntern.Repository/Persistence/AppDbContext.cs
@@ -41,6 +41,23 @@ public partial class AppDbContext : DbContext, IUnitOfWork
     //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
     //        => optionsBuilder.UseNpgsql("Host=database.nextintern.tech;Database=nextintern;Username=root;Password=iumaycauratnhiu");
 
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries())
+        {
+            if (entry.State == EntityState.Deleted)
+            {
+                entry.State = EntityState.Modified;
+                var deletedDateProperty = entry.Entity.GetType().GetProperty("DeletedDate");
+                if (deletedDateProperty is not null)
+                {
+                    deletedDateProperty.SetValue(entry.Entity, DateTime.Now);
+                }
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasPostgresExtension("uuid-ossp");
